a2467faa0a51d4bbc9aa8a45c46b41ef8f30af62
dutikovdn <dutikovdn@rus-dev.com>
2023-05-25 11:20:37 +0500

    real multi-thread test added

--- test/multithread.mjs
+++ test/multithread.mjs
@@ -1,6 +1,7 @@
 import test from 'node:test';
 import { strict as assert } from 'node:assert';
-import {DIService, SERVICE_INIT} from '../di-service.mjs';
+import { Worker } from 'node:worker_threads';
+import { DIService, SERVICE_INIT } from '../di-service.mjs';
 
 let counter = 0;
 
@@ -11,12 +12,13 @@ class TestClass {
     }
 
     async [SERVICE_INIT]() {
-        return new Promise((r) => setTimeout(() => r(), 10));
+        return new Promise(r => setTimeout(r, 42));
     }
 }
 
 
-test('getInstance is async circle safe', { only: true }, async () => {
+test('getInstance is async circle safe', async () => {
+    counter = 0;
     const services = new DIService();
     const promises = [];
     const max = 5;
@@ -29,5 +31,42 @@ test('getInstance is async circle safe', { only: true }, async () => {
     for (let i = 1; i < max; i++) {
         assert.equal(instances[0], instances[i]);
     }
+});
+
+test('getInstance is thread safe', async () => {
+    counter = 0;
+    const max = 5;
+    const w = new Worker(`
+        const { parentPort } = require('node:worker_threads');
+        let count = 0;
+        const h = setInterval(() => { 
+            parentPort.postMessage(count);
+            if (++count === ${max}) {
+                clearInterval(h);
+            } 
+        }, 12);     
+    `, { eval: true }); // eval is not safe in general, but this is just a small test
+    const services = new DIService();
+    const promises = [];
+    const subject = new class {
+        subs(s) { this.s = s }
+        next() { this.s() }
+        wait() { return new Promise((r) => this.subs(r))  }
+    }
+    w.on('message', (v) => {
+        promises.push(services.getInstance(TestClass));
+        if (v === max-1) {
+            subject.next();
+        }
+    });
+    await subject.wait();
+
+    const instances = await Promise.all(promises);
+    assert.equal(counter, 1);
+    assert.equal(instances.length, max);
+    for (let i = 1; i < max; i++) {
+        assert.equal(instances[0], instances[i]);
+    }
+
 });
 
